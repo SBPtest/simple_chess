dc1da56c644e5b478f302a4f78246921ab026239
SBPtest <70194025+SBPtest@users.noreply.github.com>
2022-11-10 18:05:46 +0700

    Update simplechess.py
    finish the piece and board class
    only chess.move left

--- simplechess.py
+++ simplechess.py
@@ -3,6 +3,9 @@ class Piece:
     def __init__(self,color,position):
         self.color=color
         self.position=position
+        
+    def _logo(self):
+        return f"."
     
 class Pawn(Piece):
     
@@ -17,6 +20,9 @@ class Pawn(Piece):
             if self.position[1]==7:
                 available_move.append([self.position[0],self.position[1]-2])
         return available_move
+    
+    def _logo(self):
+        return f"p"
     
     def __str__(self):
         return f"{self.color} Pawn in {self.position}"
@@ -43,6 +49,9 @@ class Rook(Piece):
             available_move.append([self.position[0],self.position[1]-i])
         return available_move
     
+    def _logo(self):
+        return f"R"
+    
     def __str__(self):
         return f"{self.color} Rook in {self.position}"
 
@@ -68,6 +77,9 @@ class Bishop(Piece):
             available_move.append([self.position[0]-i,self.position[1]+i])
         return available_move
     
+    def _logo(self):
+        return f"B"
+    
     def __str__(self):
         return f"{self.color} Bishop in {self.position}"
 
@@ -92,6 +104,9 @@ class Knight(Piece):
         if (self.position[0]-2)>=1 and (self.position[1]-1)>=1:
             available_move.append([self.position[0]-2,self.position[1]-1])
         return available_move
+    
+    def _logo(self):
+        return f"N"
     
     def __str__(self):
         return f"{self.color} Knight in {self.position}"
@@ -134,6 +149,9 @@ class Queen(Piece):
             available_move.append([self.position[0],self.position[1]-i])
         return available_move
     
+    def _logo(self):
+        return f"Q"
+    
     def __str__(self):
         return f"{self.color} Queen in {self.position}"
 
@@ -163,78 +181,95 @@ class King(Piece):
             available_move.append([x_move_minus,y_move_minus])
         return available_move
     
+    def _logo(self):
+        return f"K"
+    
     def __str__(self):
         return f"{self.color} King in {self.position}"
 
 class Board:
+    '''
+    "I would like to make this a custom dictionary with default initialization so that the ugly Chess.board.board['e4'] lose one of its board"
+    '''
     file="abcdefgh"
     rank="12345678"
-    board={(x+y):0 for x in file for y in rank}
-    for i in range(len(file)):
-        playboard[file[i]+'2']=Pawn('White',[i+1,2])
-        playboard[file[i]+'7']=Pawn('Black',[i+1,7])
-    playboard['a1']=Rook('White',[1,1])
-    playboard['h1']=Rook('White',[8.1])
-    playboard['b1']=Knight('White',[2,1])
-    playboard['g1']=Knight('White',[7,1])
-    playboard['c1']=Bishop('White',[3,1])
-    playboard['f1']=Bishop('White',[6,1])
-    playboard['d1']=Queen('White',[4,1])
-    playboard['e1']=King('White',[5,1])
-    playboard['a8']=Rook('Black',[1,8])
-    playboard['h8']=Rook('Black',[8,8])
-    playboard['b8']=Knight('Black',[2,8])
-    playboard['g8']=Knight('Black',[7,8])
-    playboard['c8']=Bishop('Black',[3,8])
-    playboard['f8']=Bishop('Black',[6,8])
-    playboard['d8']=Queen('Black',[4,8])
-    playboard['e8']=King('Black',[5,8])
     
-    def parse(self,move):
-        pass
-
-'''   
-def board():
-    file="abcdefgh"
-    rank="12345678"
-    playboard={(x+y):0 for x in file for y in rank}
-    for i in range(len(file)):
-        playboard[file[i]+'2']=Pawn('White',[i+1,2])
-        playboard[file[i]+'7']=Pawn('Black',[i+1,7])
-    playboard['a1']=Rook('White',[1,1])
-    playboard['h1']=Rook('White',[8.1])
-    playboard['b1']=Knight('White',[2,1])
-    playboard['g1']=Knight('White',[7,1])
-    playboard['c1']=Bishop('White',[3,1])
-    playboard['f1']=Bishop('White',[6,1])
-    playboard['d1']=Queen('White',[4,1])
-    playboard['e1']=King('White',[5,1])
-    playboard['a8']=Rook('Black',[1,8])
-    playboard['h8']=Rook('Black',[8,8])
-    playboard['b8']=Knight('Black',[2,8])
-    playboard['g8']=Knight('Black',[7,8])
-    playboard['c8']=Bishop('Black',[3,8])
-    playboard['f8']=Bishop('Black',[6,8])
-    playboard['d8']=Queen('Black',[4,8])
-    playboard['e8']=King('Black',[5,8])
-    return playboard
-
-def parse(move_input):
-    board=Board()
-    sides="White"
-    if len(move_input)==2:
+    def __init__(self):
+        file="abcdefgh"
+        rank="12345678"
+        self.board={(x+y):Piece(None,None) for x in file for y in rank}
+        for i in range(len(file)):
+            self.board[file[i]+'2']=Pawn('White',[i+1,2])
+            self.board[file[i]+'7']=Pawn('Black',[i+1,7])
+        self.board['a1']=Rook('White',[1,1])
+        self.board['h1']=Rook('White',[8,1])
+        self.board['b1']=Knight('White',[2,1])
+        self.board['g1']=Knight('White',[7,1])
+        self.board['c1']=Bishop('White',[3,1])
+        self.board['f1']=Bishop('White',[6,1])
+        self.board['d1']=Queen('White',[4,1])
+        self.board['e1']=King('White',[5,1])
+        self.board['a8']=Rook('Black',[1,8])
+        self.board['h8']=Rook('Black',[8,8])
+        self.board['b8']=Knight('Black',[2,8])
+        self.board['g8']=Knight('Black',[7,8])
+        self.board['c8']=Bishop('Black',[3,8])
+        self.board['f8']=Bishop('Black',[6,8])
+        self.board['d8']=Queen('Black',[4,8])
+        self.board['e8']=King('Black',[5,8])
         
-    elif len(move_input)==3:
-        pass
-    elif len(move_input)==4:
-        pass
-    elif len(move_input)==5:
-        pass
- '''
+    def print_board(self):
+        file="abcdefgh"
+        rank="87654321"
+        for y in rank:
+            print("|",self.board["a"+y]._logo(),"|",self.board["b"+y]._logo(),"|",self.board["c"+y]._logo(),"|",self.board["d"+y]._logo(),"|",self.board["e"+y]._logo(),"|",self.board["f"+y]._logo(),"|",self.board["g"+y]._logo(),"|",self.board["h"+y]._logo(),"|")
+  
+class Chess:
+    '''
+    
+    Need future implementation:
+    1. Castling, as in detection of legality of castling
+    2. Proper check & checkmate implementation i.e detecting the only move to escape check, and declaring checkmate 
+    if no legal move detected 
+    3. simplifying the ungodly amount of if else
+    
+    '''
+    
+    def __init__(self):
+        self.side="White"
+        self.board=Board()
+    
+    def move(self,moves):
+        if len(moves)==2:
+            if self.side=="White":
+                pass
+            else:
+                pass
+            pass
+        elif len(moves)==3:
+            if self.side=="White":
+                pass
+            else:
+                pass
+            pass
+        elif len(moves)==4:
+            if self.side=="White":
+                pass
+            else:
+                pass
+            pass
+        elif len(moves)==5:
+            if self.side=="White":
+                pass
+            else:
+                pass
+            pass
+        if self.side=="White":
+            self.side="Black"
+        else:
+            self.side=="White"
 
 if __name__=="__main__":
-    Board=board()
-    print (Board['h1'].legal_move())
-    
-    
-    
+    Board=Board()
+    Board.print_board()
+ 
